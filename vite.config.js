73ba56f5ac1fe13f04543c0bae69efb75e8e6f3b
M-Kovaleva <owlet.savvina@gmail.com>
2026-02-19 18:04:16 +0100

    Revert "Updated  package.json"
    This reverts commit 454a192a73d4c4b49fc470cc88cf5e75dbed9387.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  // ✅ Условный base: пустой для dev, с путем для production
   base: process.env.NODE_ENV === 'production' 
     ? '/marina-kovaleva-js2/' 
     : '/',
