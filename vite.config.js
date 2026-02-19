7746c865c3de6ff332b3462cc2aaaacbb78b7ca8
M-Kovaleva <owlet.savvina@gmail.com>
2026-02-19 18:10:31 +0100

    revert Bug fix - 404 page opens on demo-version site instead of Home page

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,5 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // ✅ Условный base: пустой для dev, с путем для production
-  base: process.env.NODE_ENV === 'production' 
-    ? '/marina-kovaleva-js2/' 
-    : '/',
-  
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  }
-});
+  base: '/marina-kovaleva-js2/',
+})
